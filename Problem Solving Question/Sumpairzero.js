5ae60b336d4e8499689f2013bdf53d50f85f6a2a
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-05-15 18:05:52 +0530

    Improvised sumpair added

--- Problem Solving Question/Sumpairzero.js
+++ Problem Solving Question/Sumpairzero.js
@@ -4,7 +4,7 @@
 function sum(array){
     for(let i=0;i<array.length;i++){
         // console.log(i);
-        for(let j=1;j<array.length;j++){
+        for(let j=1;j<array.length;j++){//array first value is not utlised because first start comaprioson from index 0 that s why index 1 is used here
             // console.log([array[i],array[j]]);
             if(array[i]+array[j]===0){
                 return ([array[i],array[j]]);
@@ -15,3 +15,4 @@ function sum(array){
 }
 const pair = sum([-5,-4,-3,-2,0,2,4,6,8]);
 console.log(pair);
+//quadritic time complexity 0(n^2)
---
+++ Problem Solving Question/imrovisedSumzeropair.js
@@ -0,0 +1,15 @@
+//Checking first sum pair in an sorted array is zero
+//[-5,-4,-3,-2,0,2,4,6,8] (Input)
+//[-4,4] -> output
+function Sumpair(array){
+   let  left=0;
+   let  Right=array.length -1;
+   while(left<Right){
+       let sum=array[left]+array[Right];
+       if(sum===0){
+           return [array[left],array[Right]];
+       }else sum>0 ?Right--:left++;
+   }
+}
+let pair = Sumpair([-5,-4,-3,-2,0,2,4,6,8]);
+console.log(pair)
