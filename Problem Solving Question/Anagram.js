9486ec3145f3257abdb2dde33c89c9eb89cf88a2
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-05-16 11:20:11 +0530

    Anagram code improvised

--- Problem Solving Question/Anagram.js
+++ Problem Solving Question/Anagram.js
@@ -4,10 +4,16 @@ function isAnagram(string1,string2){
         return false;
     }
     let counter={}
-    for(let letter of string1){
-
-        counter[letter]=(counter[letter] || 0) +1;
-        // console.log(counter[letter]);
+    // for(let letter of string1){
+    //     console.log(letter)
+    //     counter[letter]=(counter[letter] || 0) +1;
+    //     console.log(counter[letter]);
+    // }
+    for(let i=0;i<string1.length;i++){
+        // console.log(string1[i]);
+        let a= string1[i]
+        counter[a]=(counter[a] || 0) +1;
+        // console.log(counter[a])
     }
     for(let item of string1){
         if(!counter[item]){
